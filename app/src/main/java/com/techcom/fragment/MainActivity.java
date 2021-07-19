5331c1b599734d898c873992d2b3ac48e1290804
kimminkuk <rlaal567@gmail.com>
2021-07-19 23:55:38 +0900

    Fragment Study

---
+++ app/src/main/java/com/techcom/fragment/MainActivity.java
@@ -0,0 +1,47 @@
+package com.techcom.fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    MainFragment fragment1;
+    MenuFragment fragment2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fragment1 = new MainFragment();
+        fragment2 = new MenuFragment();
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //프래그먼트 매니저!
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
+            }
+        });
+
+        Button button3 = (Button)findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //프래그먼트 매니저!
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment2).commit();
+            }
+        });
+    }
+
+    public void onFragmentChange(int index)
+    {
+        if(index == 0) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
+        } else if (index == 1) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment2).commit();
+        }
+    }
+}
